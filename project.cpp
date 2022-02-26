dc422f9cedcd65e16dfdaf7bb199a3b12aec0e7d
hoangvm2307 <vominhhoangkhung@gmail.com>
2022-02-26 09:24:01 +0700

    Add validDate to check Payment input

--- project.cpp
+++ project.cpp
@@ -18,12 +18,10 @@ struct account {
     char accType;
     float oldBalance;
     float newBalance; 
-
-
     float payment;
     date lastPayment;
 };
-
+int validDate(int d,int m,int y);
 typedef account AC;
 
 void xoaXuongDong(char x[]){
@@ -47,16 +45,19 @@ void inputOneAccount(AC &customerAcc){
         printf("Enter previous balance: ");
         scanf("%f", &customerAcc.oldBalance);
         do {
-        printf("Enter current balance: ");
-        scanf("%f", &customerAcc.payment);
+        	printf("Enter current balance: ");
+        	scanf("%f", &customerAcc.payment);
     	} while (customerAcc.payment>customerAcc.oldBalance);
-        printf("Enter payment date: ");
-        scanf("%d/%d/%d", &customerAcc.lastPayment.day,
-         &customerAcc.lastPayment.month, &customerAcc.lastPayment.year);
-        fflush(stdin);
+		do{
+			printf("Enter payment date: ");
+			scanf("%d%d%d", &customerAcc.lastPayment.day,
+			&customerAcc.lastPayment.month, &customerAcc.lastPayment.year);
+			fflush(stdin);
+		}
+		while(validDate(customerAcc.lastPayment.day,
+			customerAcc.lastPayment.month, customerAcc.lastPayment.year) == 0);
 
 }
-
 void inputAListOfAccount(AC listCustomerAcc[], int &numOfAcc){
 	printf("Enter number of accounts you want to add: ");
     scanf("%d", &numOfAcc);
@@ -133,4 +134,14 @@ int main(){
 	}while(choice != 3);
     
 }
+int validDate(int d, int m, int y){
+    int maxd = 31;
+    if(d<1|| d>31|| m<1 || m>12) return 0;
+    if(m == 4 || m == 6 || m == 9 || m == 11) maxd = 30;
+    else if(m == 2){
+        if(y % 400 == 0 || (y % 4 == 0 && y % 100 !=0)) maxd = 29;
+        else maxd = 28;
+    }   
+    return d <= maxd;
+} 
 
