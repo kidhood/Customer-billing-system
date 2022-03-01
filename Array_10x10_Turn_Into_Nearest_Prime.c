261f2320c9934eab6fcaf09b37b642df78cb039f
hoangvm2307 <60658750+hoangvm2307@users.noreply.github.com>
2022-03-01 20:29:31 +0700

    Add files via upload

---
+++ Array_10x10_Turn_Into_Nearest_Prime.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+//HOANG DEP TRAI
+#define MAXN 100
+
+int intRandom(int min, int max);
+int checkPrime(int n);
+void initMatrix();
+void turnNearestPrime();
+void printArray();
+int main(){
+    int a[MAXN][MAXN];
+    srand(time(NULL));
+    initMatrix(a);
+    printf("Before converting:\n");
+    printArray(a);
+    turnNearestPrime(a);
+    printf("After converting:\n");
+    printArray(a);
+    return 0;
+}
+void printArray(int a[MAXN][MAXN]){
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 10; j++){
+            printf("%d\t", a[i][j]);
+        }
+        printf("\n");
+    }
+}
+void turnNearestPrime(int a[MAXN][MAXN]){
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 10; j++){
+            for(int l = 0; l < 1000 ; l++){
+                if (checkPrime(a[i][j] - l)){
+                    a[i][j] = a[i][j] - l;
+                }
+                if (a[i][j] <= INT_MAX - l && checkPrime(a[i][j] + l)){
+                    a[i][j] = a[i][j] + l;
+                }
+            }
+        }
+    }
+}
+
+void initMatrix(int a[MAXN][MAXN]){
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 10; j++){
+            a[i][j] = intRandom(1,100);  
+        }
+    }
+}
+int intRandom(int min, int max){
+    int number = (rand() % ((max + 1) - min) + min);
+    return number;
+}
+int checkPrime(int n){
+    int flag;
+    if(n == 0 || n == 1){
+        flag = 1;
+    }
+    for(int i = 2; i <= (n/2); i++){
+        if(n % i == 0){
+            flag = 1;
+        }
+        else flag = 0;
+    }
+    if(flag == 1){
+        return 0;
+    }
+    if(flag == 0){
+        return 1;
+    }
+}
+ 
